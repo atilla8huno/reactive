f35140a05e843166957f25b01f5ecfdffa630cdb
Atilla Barros <atilla8huno@gmail.com>
2023-10-02 10:30:02 +0100

    Removed email from players table

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -11,9 +11,7 @@ CREATE TABLE IF NOT EXISTS players
     id      SERIAL,
     name    VARCHAR(255),
     number  INT,
-    email   VARCHAR(255),
     team_id INT,
     CONSTRAINT pk_players_id PRIMARY KEY (id),
-    CONSTRAINT uq_players_email UNIQUE (email),
     CONSTRAINT fk_players_team_id FOREIGN KEY (team_id) REFERENCES teams (id)
 );
